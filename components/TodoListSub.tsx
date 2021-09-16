a25e925124adbb21590d847af3d35283a76b4c4f
kigpand <kigpand@naver.com>
2021-09-16 16:22:40 +0900

    calendar 추가, recoil 상태값 적용

--- components/TodoListSub.tsx
+++ components/TodoListSub.tsx
@@ -1,14 +1,46 @@
 import styled from 'styled-components';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { todoListArray } from '../recoil/recoil';
 
 const SubStyled = styled.div`
-    width : 500px;
-    height : 700px;
-    background-color : #EDFF75;
+    width : 400px;
+    height : 600px;
     z-index : 1;
+    position : relative;
+
+    .addCalendar{
+        border-radius : 4px;
+        background-color : white;
+    }
+
+    .calendar{
+        position : absolute;
+    }
 `;
 const TodoListSub = () =>{
+
+    const [onCalendar, setOnCalendar] = useState<boolean>(false);
+    const [todoList, setTodoList] = useRecoilState(todoListArray);
+
+    const onToggleCalendar = () =>{
+        setOnCalendar(!onCalendar);
+    }
+
+    const onChangeDate = (e: Date) =>{
+        setTodoList({ date : e, item : []})
+    }
+
     return(
-        <SubStyled>투두리스트 서브</SubStyled>
+        <SubStyled>
+            <div className="addCalendar" onClick={onToggleCalendar}>달력 보기</div>
+            { onCalendar && <div className="calendar">
+                <Calendar onChange={onChangeDate} value={todoList.date}/>
+            </div>}
+            <div>테스트</div>
+        </SubStyled>
     )
 }
 
